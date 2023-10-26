abde8a4e6c4df1ad3dbb17465b5d463a0b416997
n.carbo@human-immobilier.fr <n.carbo@human-immobilier.fr>
2023-10-26 13:30:36 +0200

    init

---
+++ BackBAI/Controllers/APIController.cs
@@ -0,0 +1,109 @@
+﻿using BackBAI.Models;
+using BackBAI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackBAI.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+
+    public class ApiController : ControllerBase
+    {
+        private ideeContext _context;
+        private BoiteAideesServices _boiteAideesServices;
+
+        public ApiController(ideeContext context, BoiteAideesServices boiteAideesServices)
+        {
+            _context = context;
+            _boiteAideesServices = boiteAideesServices;
+        }
+
+        [HttpGet(Name = "GetIdea")]
+        public IActionResult GetIdees()
+        {
+            var result = _boiteAideesServices.Get();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult PostIdea([FromBody] Idea idea)
+        {
+            try
+            {
+                //idea.Id = 0; 
+                idea.CreatedAt = DateTime.Now;
+                _context.Idea.Add(idea);
+                _context.SaveChanges();
+
+                return Ok("L'idée a été ajoutée avec succès.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erreur lors de l'ajout de l'idée : {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteIdea(int id)
+        {
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    // Supprime les likes associés à l'idée
+                    _context.Database.ExecuteSqlRaw("DELETE FROM Likes WHERE FkIdeaIdLikes = {0}", id);
+
+                    // Supprime les commentaires associés
+                    _context.Comment.RemoveRange(_context.Comment.Where(c => c.FkIdeaId == id));
+
+                    // Supprime l'idée
+                    var idea = _context.Idea.Find(id);
+                    if (idea != null)
+                    {
+                        _context.Idea.Remove(idea);
+                    }
+
+                    _context.SaveChanges();
+
+                    transaction.Commit();
+
+                    return NoContent(); // Retourne une réponse 204 (No Content) si la suppression est réussie.
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    return StatusCode(500); // Une erreur s'est produite lors de la suppression.
+                }
+            }
+        }
+        // faut savoir comment faire la table like, est ce que tu laisses la jointure bizarre, est ce que tu fais une table ?
+        // car erreur sur méthode Delete, impossible de supprimer l'idée a cause des autres idea dans d'autre table (fk_idea...)
+
+
+    }
+
+}
+
+// JSON POUR LE POST
+//{
+//    "title": "Mon idée",
+//  "description": "Description de mon idée",
+//  "createdAt": "2023-10-24T17:32:58.195Z",
+//  "updatedAt": "2023-10-24T17:32:58.195Z",
+//  "fkUsersId": 1, 
+//  "comment": [
+//    {
+//        "text": "Commentaire sur mon idée",
+//      "createdAt": "2023-10-24T17:32:58.195Z",
+//      "updatedAt": "2023-10-24T17:32:58.195Z",
+//      "fkUsersIdComment": 2
+//    }
+//  ],
+//  "category": [
+//    {
+//        "name": "Nom de la catégorie"
+//    }
+//  ]
+//}
---
+++ BackBAI/Services/BoiteAideesServices.cs
@@ -0,0 +1,113 @@
+﻿using BackBAI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackBAI.Services
+{
+    public class BoiteAideesServices : Controller
+    {
+        private readonly ideeContext _context;
+
+        public BoiteAideesServices(ideeContext context)
+        {
+            _context = context;
+        }
+        // GET: BoiteAideesServices
+        public IEnumerable<Idea> Get()
+        {
+            var result = _context.Idea.ToList();
+            return result;
+        }
+
+        //public bool DeleteById(int id)
+        //{
+        //    var idea = _context.Idea.Find(id);
+
+        //    if (idea == null)
+        //    {
+        //        return false;
+        //    }
+
+        //    var commentsToDelete = _context.Comment.Where(c => c.FkIdeaId == id);
+        //    var likesToDelete = _context.Likes.Where(c => c.FkIdeaIdLikes == id);
+
+        //    _context.Comment.RemoveRange(commentsToDelete);
+        //    _context.Likes.RemoveRange(likesToDelete);
+        //    _context.SaveChanges();
+
+        //    _context.Idea.Remove(idea);
+        //    _context.SaveChanges();
+
+        //    return true;
+        //}
+
+
+
+        //// GET: BoiteAideesServices/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: BoiteAideesServices/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: BoiteAideesServices/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: BoiteAideesServices/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: BoiteAideesServices/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        // GET: BoiteAideesServices/Delete/5
+
+
+        //// POST: BoiteAideesServices/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
